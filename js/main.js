22c23a242154555f11443c91f8d54a47f951e06f
Philip Rideout <philip@napofearth.com>
2015-09-06 22:36:06 -0700

    Misc updates to the page.

--- js/main.js
+++ js/main.js
@@ -6,16 +6,6 @@ heman.Buffer.prototype.data = function() {
     return heman.HEAPF32.subarray(this.begin(), this.end());
 };
 
-GIZA.refreshSize = function() {
-    var $container = $('.canvas-container');
-    var w = $container.width(), h = $container.height();
-    GIZA.canvas.width = w * window.devicePixelRatio;
-    GIZA.canvas.height = h * window.devicePixelRatio;
-    GIZA.canvas.style.width = w + 'px';
-    GIZA.canvas.style.height = h + 'px';
-    GIZA.aspect = w / h;
-};
-
 $('#menu-toggle').click(function() {
     $('nav').toggle();
     GIZA.refreshSize();
@@ -24,6 +14,7 @@ $('#menu-toggle').click(function() {
 $(function() {
 
     var SIZE = 512;
+    var DOFIT = true;
     var GL = GIZA.init();
     var M4 = GIZA.Matrix4;
     var C4 = GIZA.Color4;
@@ -33,11 +24,58 @@ $(function() {
     GL.getExtension('OES_texture_float');
     GL.getExtension('OES_texture_float_linear');
 
+    var attribs = {
+        POSITION: 0,
+        TEXCOORD: 1,
+    };
+
+    var programs = GIZA.compile({
+        color: {
+          vs: ['simplevs'],
+          fs: ['colorfs'],
+          attribs: {
+            Position: attribs.POSITION,
+            TexCoord: attribs.TEXCOORD,
+          }
+        },
+        gray: {
+          vs: ['simplevs'],
+          fs: ['grayfs'],
+          attribs: {
+            Position: attribs.POSITION,
+            TexCoord: attribs.TEXCOORD,
+          }
+        }
+    });
+
     var vertexBuffer = GL.createBuffer();
+    var gradient = null;
     var texture = null;
+    var dirty = false;
+    var program = programs.gray;
+
+    GIZA.refreshSize = function() {
+        var w, h;
+        if (DOFIT) {
+            w = h = SIZE / window.devicePixelRatio;
+        } else {
+            var $container = $('.canvas-container');
+            w = $container.width();
+            h = $container.height();
+        }
+        GIZA.canvas.width = w * window.devicePixelRatio;
+        GIZA.canvas.height = h * window.devicePixelRatio;
+        GIZA.canvas.style.width = w + 'px';
+        GIZA.canvas.style.height = h + 'px';
+        GIZA.aspect = w / h;
+        dirty = true;
+    };
+
+    var refresh = function() {
+        GIZA.refreshSize();
+    };
 
     $('#perlin').click(function() {
-        window.console.log('Generating noise...');
         var frequency = 4;
         var amplitude = 1;
         var octaves = 10;
@@ -46,13 +84,14 @@ $(function() {
         var seed = Math.floor(Math.random() * 1000);
         var elev = heman.Generate.simplex_fbm(
             SIZE, SIZE, frequency, amplitude, octaves, lacunarity, gain, seed);
-        window.console.log(elev.width(), elev.height(), elev.nbands());
         texture = texture || GL.createTexture();
         GL.bindTexture(GL.TEXTURE_2D, texture);
         GL.texImage2D(GL.TEXTURE_2D, 0, GL.LUMINANCE, elev.width(), elev.height(), 0, GL.LUMINANCE, GL.FLOAT, elev.data());
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
         heman.Image.destroy(elev);
+        program = programs.gray;
+        refresh();
     });
 
     $('#island').click(function() {
@@ -66,6 +105,8 @@ $(function() {
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
         heman.Image.destroy(elev);
+        program = programs.color;
+        refresh();
     });
 
     $('#lighting').click(function() {
@@ -81,65 +122,63 @@ $(function() {
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
         heman.Image.destroy(rgb);
+        program = programs.gray;
+        refresh();
     });
 
-    var attribs = {
-        POSITION: 0,
-        TEXCOORD: 1,
-    };
-
-    var programs = GIZA.compile({
-        simple: {
-          vs: ['simplevs'],
-          fs: ['simplefs'],
-          attribs: {
-            Position: attribs.POSITION,
-            TexCoord: attribs.TEXCOORD,
-          }
-        }
+    // Set up a description of the vertex format.
+    var bufferView = new GIZA.BufferView({
+      p: [Float32Array, 2],
+      t: [Float32Array, 2],
     });
 
-    var init = function() {
+    // Allocate and populate the ArrayBuffer.
+    var vertexArray = bufferView.makeBuffer(4);
+    var iterator = bufferView.iterator();
 
-        // Set up a description of the vertex format.
-        var bufferView = new GIZA.BufferView({
-          p: [Float32Array, 2],
-          t: [Float32Array, 2],
-        });
+    var vertex;
+    vertex = iterator.next(); V2.set(vertex.p, [-1, -1]); V2.set(vertex.t, [0, 0]);
+    vertex = iterator.next(); V2.set(vertex.p, [-1, 1]); V2.set(vertex.t, [0, 1]);
+    vertex = iterator.next(); V2.set(vertex.p, [1, -1]); V2.set(vertex.t, [1, 0]);
+    vertex = iterator.next(); V2.set(vertex.p, [1, 1]); V2.set(vertex.t, [1, 1]);
 
-        // Allocate and populate the ArrayBuffer.
-        var vertexArray = bufferView.makeBuffer(4);
-        var iterator = bufferView.iterator();
+    // Create the vertex buffer object etc.
+    GL.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer);
+    GL.bufferData(GL.ARRAY_BUFFER, vertexArray, GL.STATIC_DRAW);
+    GL.clearColor(0.6, 0.6, 0.6, 1.0);
+    GL.enable(GL.BLEND);
+    GL.blendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
 
-        var vertex;
-        vertex = iterator.next(); V2.set(vertex.p, [-1, -1]); V2.set(vertex.t, [0, 0]);
-        vertex = iterator.next(); V2.set(vertex.p, [-1, 1]); V2.set(vertex.t, [0, 1]);
-        vertex = iterator.next(); V2.set(vertex.p, [1, -1]); V2.set(vertex.t, [1, 0]);
-        vertex = iterator.next(); V2.set(vertex.p, [1, 1]); V2.set(vertex.t, [1, 1]);
+    var mv = M4.scale(DOFIT ? 1.0 : 0.8);
 
-        // Create the vertex buffer object etc.
-        GL.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer);
-        GL.bufferData(GL.ARRAY_BUFFER, vertexArray, GL.STATIC_DRAW);
-        GL.clearColor(0.6, 0.6, 0.6, 1.0);
-        GL.enable(GL.BLEND);
-        GL.blendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
+    var img = new Image();
+    img.src = 'img/terrain.png';
+    img.onload = function() {
+        gradient = GL.createTexture();
+        GL.bindTexture(GL.TEXTURE_2D, gradient);
+        GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA,
+            GL.UNSIGNED_BYTE, img);
+        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
+        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
+        dirty = true;
     };
 
     var draw = function(currentTime) {
-
-        if (!texture) {
+        if (!texture || !dirty || !gradient) {
             return;
         }
+        dirty = false;
 
         var proj = M4.orthographic(
             -GIZA.aspect, GIZA.aspect, // left right
             -1, +1, // bottom top
             0, 100);  // near far
 
-        var program = programs.simple;
-        var mv = M4.rotationZ(0.01 * Math.sin(currentTime * 0.01));
-        mv = M4.multiply(mv, M4.scale(0.8));
-
+        GL.activeTexture(GL.TEXTURE1);
+        GL.bindTexture(GL.TEXTURE_2D, gradient);
+        GL.activeTexture(GL.TEXTURE0);
+        GL.bindTexture(GL.TEXTURE_2D, texture);
         GL.clear(GL.COLOR_BUFFER_BIT);
         GL.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer);
         GL.enableVertexAttribArray(attribs.POSITION);
@@ -149,9 +188,9 @@ $(function() {
         GL.useProgram(program);
         GL.uniformMatrix4fv(program.projection, false, proj);
         GL.uniformMatrix4fv(program.modelview, false, mv);
+        GL.uniform1i(program.gradient, 1);
         GL.drawArrays(GL.TRIANGLE_STRIP, 0, 4);
     };
 
-    init();
     GIZA.animate(draw);
 });
